8bc84c40635559ce6f4bf88cf781586af70a7f91
teezeit <tobias.hoelzer@gmail.com>
2017-03-10 19:13:20 +0100

    changed example

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # tuning_xgboost
+
+## Minimal Example
+```
+import numpy as np
+import xgboost as xgb
+import tuning_xgboost
+X_train		 = np.random.rand(100,4)
+y_train		 = np.random.randint(2,size=100)
+grid1  		 = {'n_estimators' : [50, 100, 200], 'learning_rate' : [0.1, 0.2, 0.3]}
+grid2 		 = {'max_depth' : [2,3,4], 'min_child_weight' : [4,5]}
+grid3 		 = {'colsample_bylevel' : [0.7, 0.8, 0.9], 'subsample' : [0.5, 0.6, 0.7]}
+grid4 		 = {'scale_pos_weight' : [1,2,5]}
+hyperlist_to_try = [grid1, grid2,grid3,grid4]
+booster 	 = xgb.XGBClassifier()
+
+tuned_estimator  = tuning_xgboost.grid_search_tuning(X_train,y_train,hyperlist_to_try,booster)
+tuned_parameter  = tuned_estimator.get_params()
+#you also pass more parameters to GridSearchCV of course.
+```
+* The function will first run grid GridSearchCV on grid1, store the best hyperparameters, then proceed to run GridSearchCV on grid2, store the best parameters from grid1 and grid2, then proceed to run GridSearchCV on grid3, and so on. The overall best booster be returned in the end.
+* If you want, the parameters and the results will be plotted (if you don't optimize more than 2 at the same time)
+
 ## What is this for?
 [tuning_xgboost.py](https://github.com/teezeit/tuning_xgboost/blob/master/tuning_xgboost.py) helps you using consecutive (greedy) gridsearch with cross validation to tune xgboost hyperparameters in python. This is useful, when the hyperparameter space is too big or you don't really want to dig too deep into how this all works.
 I recommend using pairwise gridsearch. If you have no idea what you are doing, use those pairs:
@@ -19,30 +41,9 @@ Just define a list of values you like to try and give it to tuning_xgboost.grid_
 
 All information and part of the code is taken from [Jesse Steinweg-Woods](https://jessesw.com/XG-Boost/), [Jason Brownlee](http://machinelearningmastery.com/tune-learning-rate-for-gradient-boosting-with-xgboost-in-python/) and [Aarshay Jain](https://www.analyticsvidhya.com/blog/2016/03/complete-guide-parameter-tuning-xgboost-with-codes-python/). Thank you!
 
+[example_tuning_xgboost.py](https://github.com/teezeit/tuning_xgboost/blob/master/example_tuning_xgboost.py) shows more working examples.
 
 
-## Usage
-```
-tuned_estimator      = tuning_xgboost.grid_search_tuning(X_train,y_train,hyperparameter_grids,booster)
-tuned_parameters     = tuned_estimator.get_params()
-```
-
-[example_tuning_xgboost.py](https://github.com/teezeit/tuning_xgboost/blob/master/example_tuning_xgboost.py) shows a minimal working example.
-
-
-
-
-1. Define the hyperparameters in dictionaries that you want to tune
-  * `grid1 = {'n_estimators' : [50, 100, 200], 'learning_rate' : [0.1, 0.2, 0.3]}`
-  * `grid2 = {'max_depth' : [2,3,4], 'min_child_weight' : [4,5]}`
-2. Put them into a list
-  * `hyperlist = [grid1, grid2]`
-3. Define your booster
-  * `xgb.XGBClassifier()`
-4. Run the consecutive gridsearch
-  * `tuned_estimator      = tuning_xgboost.grid_search_tuning(X_train,y_train,hyperparameter_grids,booster)`
-  * The function will first run grid GridSearchCV on grid1, store the best hyperparameters then proceed to run GridSearchCV on grid2 with the best parameters from grid1. The overall best booster is then returned.
-  * If you want, the parameters and the results will be plotted (if you don't optimize more than 2 at the same time)
 
 
 
--- example_tuning_xgboost.py
+++ example_tuning_xgboost.py
@@ -4,19 +4,12 @@ March 2016
 tobias hoelzer
 This script optimizes parameters for xgboost using greeedy gridsearch + crossvalidation
 """
-import pandas as pd
+# Imports
 import numpy as np
-import tuning_xgboost
 import xgboost as xgb
-############################################################################
+import tuning_xgboost
 
-#1. create toy dataset 
-header               = ['F1','F2','F3','F4','label']
-df_train             = pd.DataFrame(np.random.rand(100,5),columns=header)
-df_train['label']    = df_train['label'].apply(lambda x: 0. if x<0.5 else 1)
-
-
-#2. define gridsearch parameters
+#. define gridsearch parameters
 #   a) model complexity
         #i)  max_depth
         #ii) min_child_weight
@@ -29,50 +22,38 @@ df_train['label']    = df_train['label'].apply(lambda x: 0. if x<0.5 else 1)
 #   d) weighting positive data
         #i) scale_pos_weight
 
-#parameter grid 1
-n_estimators        = [50, 75, 100]
-learning_rate       = [0.1, 0.2, 0.3]
-param_grid_1        = dict(n_estimators=n_estimators,learning_rate=learning_rate)
-#parameter grid 2
-max_depth           = [3, 4]
-min_child_weight    = [1, 2]
-param_grid_2        = dict(max_depth=max_depth,min_child_weight=min_child_weight)
-#paremeter grid 3
-colsample_bylevel   = [0.7, 0.8, 0.9]
-subsample           = [0.5, 0.6, 0.7]
-param_grid_3        = dict(colsample_bylevel=colsample_bylevel, subsample=subsample)
-#parameter grid 4
-scale_pos_weight    = [1,2,3,4]
-param_grid_4        = dict(scale_pos_weight=scale_pos_weight)
+#Training data + label
+X_train		 = np.random.rand(100,4)
+y_train		 = np.random.randint(2,size=100)
 
+#Grids
+grid1  		 = {'n_estimators' : [50, 100, 200], 'learning_rate' : [0.1, 0.2, 0.3]}
+grid2 		 = {'max_depth' : [2,3,4], 'min_child_weight' : [4,5]}
+grid3 		 = {'colsample_bylevel' : [0.7, 0.8, 0.9], 'subsample' : [0.5, 0.6, 0.7]}
+grid4 		 = {'scale_pos_weight' : [1,2,5]}
+hyperlist_to_try = [grid1, grid2,grid3,grid4]
 
-#3. prepare for search
-X_train              = df_train.loc[:,header[:-1]].values
-y_train              = df_train.loc[:,header[ -1]].values
-hyperparameter_grids = [param_grid_1,param_grid_2,param_grid_3,param_grid_4]
-booster              = xgb.XGBClassifier()
+#Booster
+booster 	      = xgb.XGBClassifier()
 
-#4 a) Run tuning
+###############################################################################
+# Now run
 print 'Run Simple Parameter Tuning\n________'
-tuned_estimator      = tuning_xgboost.grid_search_tuning(X_train,y_train,hyperparameter_grids,booster)
-tuned_parameters     = tuned_estimator.get_params()
+tuned_estimator   = tuning_xgboost.grid_search_tuning(X_train,y_train,hyperlist_to_try,booster)
+tuned_parameters  = tuned_estimator.get_params()
 
 for parameter in  tuned_parameters:
     print parameter, '\t\t',tuned_parameters[parameter]
 
+# Define additional parameters
 
-
-
-
-#4 b).  additional parameters
-
-print '\n\n Run Parameter Tuning with extra parameters\n________'
+print '\n\n Run Parameter Tuning with extra parameters given to GridSearchCV\n________'
 gridsearch_params = {
                     'cv'        : 2,
                     'scoring'   : 'roc_auc',#'roc_auc', 'average_precision'
                     'verbose'   : 2
                     }
-tuned_estimator     = tuning_xgboost.grid_search_tuning(X_train,y_train,hyperparameter_grids,booster,gridsearch_params,verbose=False,plotting=True)
+tuned_estimator     = tuning_xgboost.grid_search_tuning(X_train,y_train,hyperlist_to_try,booster,gridsearch_params,verbose=False,plotting=True)
 tuned_parameters    = tuned_estimator.get_params()
 
 for parameter in  tuned_parameters:
